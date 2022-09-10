4d0285157babb5a12b3441b53406c9c1dc1a1d19
Nataly <natalycarvalho.dev@gmail.com>
2022-09-10 17:18:57 -0300

    primeiro commit

---
+++ src/main/java/com/cosmocompany/SaoJoseDelivery/SaoJoseDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.cosmocompany.SaoJoseDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaoJoseDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaoJoseDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosmocompany/SaoJoseDelivery/SaoJoseDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmocompany.SaoJoseDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaoJoseDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
